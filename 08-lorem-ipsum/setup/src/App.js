de2087b039e7e1d09b2e9487937ca8937723a252
roobana <roobanachinraj@gmail.com>
2024-06-25 21:10:39 +0530

    app 8 completed

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -2,20 +2,36 @@ import React, { useState } from "react";
 import data from "./data";
 function App() {
   const [content, setContent] = useState([]);
-  const [inputIndex, setIndex] = useState(1);
-  const handleClick = () => {};
-  setContent(data[index]);
+  const [inputIndex, setIndex] = useState(0);
+  const handleClick = (e) => {
+    e.preventDefault();
+    const index = Number(inputIndex);
+    let str = [];
+    for (let i = 0; i < index; i++) {
+      str.push(data[i]);
+    }
+    setContent(str);
+  };
+
   return (
     <main>
       <div>
         <h3>TIRED OF BORING LOREM IPSUM?</h3>
-        <form className="lorem-form">
+        <form className="lorem-form" onSubmit={(e) => handleClick(e)}>
           <label>Paragraphs:</label>
-          <input type="number" value="1" onChange={()=>}>
-          <button className="btn" onClick={handleClick}>
-            Generate
-          </button>
+          <input
+            type="number"
+            value={inputIndex}
+            min="1"
+            onChange={(e) => setIndex(e.target.value)}
+          />
+          <button className="btn">Generate</button>
         </form>
+        <div className="result">
+          {content.map((val) => {
+            return <p>{val}</p>;
+          })}
+        </div>
       </div>
     </main>
   );
