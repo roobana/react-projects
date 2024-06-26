f4161ca56a15b334e7773108786b9e1acfb68874
roobana <roobanachinraj@gmail.com>
2024-06-26 11:40:07 +0530

    List app 10 completed

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [inputValue, setInputValue] = useState("");
+  const [listOfItems, setListOfItems] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.trim()) {
+      let temp = {
+        value: inputValue,
+        strike: false,
+      };
+      setListOfItems([...listOfItems, temp]);
+      setInputValue("");
+    }
+  };
+
+  return (
+    <main>
+      {/* <h2>grocery bud setup</h2> */}
+      {/* <div className=""> */}
+      <form className="form-control" onSubmit={handleSubmit}>
+        <div>
+          <input
+            // className="grocery"
+            type="text"
+            placeholder="please Add the items"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button>Submit</button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List listOfItems={listOfItems} setListOfItems={setListOfItems} />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,47 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React, { useState } from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+const List = ({ listOfItems, setListOfItems }) => {
+  const handleDelete = (ind) => {
+    const updateList = listOfItems.filter((val, index) => {
+      return index !== ind;
+    });
+    setListOfItems(updateList);
+  };
 
-export default List
+  const handleClick = (index) => {
+    let tempList = [...listOfItems];
+    tempList[index].strike = !tempList[index].strike;
+    setListOfItems(tempList);
+  };
+  return (
+    <div className="grocery-item">
+      <ul>
+        {listOfItems.map((item, index) => {
+          return (
+            <div className="grocery-item">
+              <li
+                style={{
+                  textDecoration: item.strike ? "line-through" : "none",
+                }}
+                key={index}
+              >
+                <input
+                  type="checkbox"
+                  value={item.strike}
+                  onClick={() => handleClick(index)}
+                />
+
+                {item.value}
+                <button onClick={() => handleDelete(index)}>
+                  <FaTrash />
+                </button>
+              </li>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default List;
